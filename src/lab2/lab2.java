62e97075785e7e9a4cc0fe7367850da136dd97ff
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-14 23:04:25 +0300

    .

--- src/lab2/lab2.java
+++ src/lab2/lab2.java
@@ -11,7 +11,13 @@ public class lab2 {
         //DivFiveThree();
 
         //task3: вычислить сумму двух цлых чисел и вернуть true, если сумма равна третьему
-        Addition();
+        //Addition();
+
+        //task4: принимает от пользователя три целых числа и возвращает true, если второе число больше первого числа, а третье число больше второго числа
+        //Comparison();
+
+        //task5: является ли число 3 как первый или последний элемент массива целых чисел
+        ArrayCheck();
     }
 
     public static void CountOdd() {
@@ -51,6 +57,41 @@ public class lab2 {
 
         if (num1 + num2 == num3) {
         System.out.println("Результат: true");
+        }
+        else {
+            System.out.println("Результат: false");
+        }
+    }
+    public static void Comparison(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введие первое число:");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int num2 = scanner.nextInt();
+        System.out.println("Введите третье число: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 < num2 && num3 > num2) {
+            System.out.println("Результат: true");
+        }
+        else {
+            System.out.println("Результат: false");
+        }
+    }
+    public static void ArrayCheck(){
+        int[] numArray = new int[4];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите четыре числа:");
+        for (int i = 0; i< numArray.length; i++){
+            numArray[i] = scanner.nextInt();
+        }
+        String a = "";
+        for (int i=0; i<4; i++) {
+            a += numArray[i] + " ";
+            System.out.println("array:" + a);
+        }
+        if (numArray[0] == 3 || numArray[3] == 3){
+            System.out.println("Результат: true");
         }
         else {
             System.out.println("Результат: false");
