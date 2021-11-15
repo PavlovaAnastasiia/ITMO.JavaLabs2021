00bfebfb579be266fddd0bd273a72f622c3bf999
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-15 20:26:24 +0300

    .

--- src/lab3/lab3.java
+++ src/lab3/lab3.java
@@ -7,16 +7,24 @@ public class lab3 {
 
     public static void main(String[] args) {
         //task1: отсортирован ли массив по возрастанию
-        //IncSort(CreateArr());
+        //IncSort1(CreateArr());
 
         //task2: считывает длину массива
-        ArrLength(new int[] {1, 2, 3, 4});
+        //ArrLength(CreateArr());
+
+        //task3: меняет местами первый и последний элемент массива
+        //SwitchFirstAndLast(CreateArr());
+
+        //task4: найти первое уникальное число
+
+
     }
 
     public static int[] CreateArr() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значения массива:");
+        System.out.println("Введите количество значений массива:");
         int size = scanner.nextInt();
+        System.out.println("Введите значения массива:");
         int[] myArray = new int[size];
 
         for (int i = 0; i < myArray.length; i++) {
@@ -24,6 +32,7 @@ public class lab3 {
         }
         return myArray;
     }
+
     public static void IncSort(int[] myArray){ //1 вариант 1 задания
         for (int i = 0; i< myArray.length; i++){
             int min = myArray[i]; //предполагаем, что первый элемент самый маленький
@@ -57,9 +66,21 @@ public class lab3 {
             System.out.println("OK");
         }
     }
-    public static void ArrLength(int[] myArray){
+    public static void ArrLength(int[] myArray){ // второе задание
         System.out.println("Array length: " + myArray.length);
         System.out.println("Result: " + Arrays.toString(myArray));
+    }
+    public static void SwitchFirstAndLast(int[] myArray){
+        System.out.println("Array 1: "+ Arrays.toString(myArray));
+
+        int var = myArray[0];
+        myArray[0] = myArray[myArray.length-1];
+        myArray[myArray.length-1] = var;
+
+        System.out.println("Array 2:" + Arrays.toString(myArray));
+    }
+    public static void firstUniqueVar (int[] myArray){
+
     }
 }
 
