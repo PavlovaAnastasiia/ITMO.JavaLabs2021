4324f68770325767532110dab2ea0e0a8ab209f8
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-17 00:24:10 +0300

    .

--- src/lab5/Vector.java
+++ src/lab5/Vector.java
@@ -1,5 +1,7 @@
 package lab5;
 
+import java.util.Random;
+
 public class Vector { //описывает вектор в трехмерном пространстве
 
     int x;
@@ -30,6 +32,7 @@ public class Vector { //описывает вектор в трехмерном 
 
         //task6: метод для суммы и разности
         SumNeg(vector1,vector2);
+
     }
 
     public static double vectLength(Vector vector1){
