c44021bb5dfc925c308b179767c3ffcaf4b33bb5
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-16 23:45:57 +0300

    .

--- src/lab5/Vector.java
+++ src/lab5/Vector.java
@@ -21,29 +21,36 @@ public class Vector { //описывает вектор в трехмерном 
         //System.out.println(scalarMult());
 
         //task4: векторное произведение с другим вектором
-        //twoVectMult();
-    }
+        /*Vector vector1 = new Vector(1,2,2);
+        Vector vector2 = new Vector(2,4,7);
+        Vector.twoVectMult(vector1, vector2);*/
 
-    public static void vectLength(int x, int y, int z){
-        double length = Math.sqrt(x*x + y*y + z*z);
-        System.out.println("Длина вектора: " + length);
-    }
-
-    public static int scalarMult(){
+        //task5: косинус угла между векторами
         Vector vector1 = new Vector(1,2,2);
         Vector vector2 = new Vector(2,4,7);
+        cosCorner(vector1,vector2);
+    }
 
+    public static double vectLength(Vector vector1){
+
+        double length = Math.sqrt(vector1.x*vector1.x + vector1.y*vector1.y + vector1.z*vector1.z);
+        System.out.println("Длина вектора: " + length);
+        return length;
+    }
+
+    public static int scalarMult(Vector vector1, Vector vector2){
         int multiplication = vector1.x* vector2.x + vector1.y* vector2.y + vector1.z* vector2.z;
         return multiplication;
     }
-    public static void twoVectMult(){
-        Vector vector1 = new Vector(1,2,2);
-        Vector vector2 = new Vector(2,4,7);
+    public static void twoVectMult(Vector vector1, Vector vector2){
 
         int mult1 = vector1.y*vector2.z - vector1.z* vector2.y;
         int mult2 = vector1.z* vector2.x - vector1.x* vector2.z;
         int mult3 = vector1.x* vector2.y - vector1.y* vector2.x;
         System.out.println("("+ mult1 + ", "+mult2+ ", " +mult3 + ")");
     }
-    public static
+    public static void cosCorner(Vector vector1, Vector vector2) {
+        double a = scalarMult(vector1, vector2) / (vectLength(vector1) * vectLength(vector2));
+        System.out.println(a);
+    }
 }
