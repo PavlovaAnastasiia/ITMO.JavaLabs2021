01ca07a8ba39c9a26fbb831ec127203fa7c77b39
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-16 23:10:10 +0300

    .

--- src/lab5/Vector.java
+++ src/lab5/Vector.java
@@ -19,6 +19,9 @@ public class Vector { //описывает вектор в трехмерном 
 
         //task3:расчет скалярного произведения
         //System.out.println(scalarMult());
+
+        //task4: векторное произведение с другим вектором
+        //twoVectMult();
     }
 
     public static void vectLength(int x, int y, int z){
@@ -29,8 +32,18 @@ public class Vector { //описывает вектор в трехмерном 
     public static int scalarMult(){
         Vector vector1 = new Vector(1,2,2);
         Vector vector2 = new Vector(2,4,7);
+
         int multiplication = vector1.x* vector2.x + vector1.y* vector2.y + vector1.z* vector2.z;
         return multiplication;
     }
-    
+    public static void twoVectMult(){
+        Vector vector1 = new Vector(1,2,2);
+        Vector vector2 = new Vector(2,4,7);
+
+        int mult1 = vector1.y*vector2.z - vector1.z* vector2.y;
+        int mult2 = vector1.z* vector2.x - vector1.x* vector2.z;
+        int mult3 = vector1.x* vector2.y - vector1.y* vector2.x;
+        System.out.println("("+ mult1 + ", "+mult2+ ", " +mult3 + ")");
+    }
+    public static
 }
