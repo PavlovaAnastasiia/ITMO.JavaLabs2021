1fd8e639775337cba1fdca82d63f9b80209f2138
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2021-11-16 20:48:56 +0300

    .

--- src/lab4/lab4.java
+++ src/lab4/lab4.java
@@ -1,9 +1,17 @@
 package lab4;
 
+import java.util.Scanner;
+
 public class lab4 {
     public static void main(String[] args){
         //task1: поиск самой длинной строки
-        longestLine();
+        //longestLine();
+
+        //tak2: является ли слово полиндромом
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите слово: ");
+        String  word = sc.next();
+        isPalindrome1(word);
 
     }
     public static void longestLine(){
@@ -18,6 +26,18 @@ public class lab4 {
             if(words[i].length()==longestString){
                 System.out.println(words[i]);
             }
+        }
+    }
+    public static void isPalindrome1(String word){
+        String reverse = "";
+        for (int i = word.length()-1; i >= 0; i--){
+            reverse = reverse + word.charAt(i); //возвращает символ, расположенный рл указанному индексу строки
+        }
+        if(word.equals(reverse)){
+            System.out.println("Слово - палиндром");
+        }
+        else{
+            System.out.println("Слово не яляется палиндромом");
         }
     }
 }
